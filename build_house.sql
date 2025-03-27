ae37ee1e26b915a2d06233050dbcc13dbf1852bf
Legion <aquamirgaming@gmail.com>
2025-03-27 23:53:06 +0300

    +more bd

--- build_house.sql
+++ build_house.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: MySQL-8.2
--- Время создания: Мар 27 2025 г., 23:40
+-- Время создания: Мар 27 2025 г., 23:52
 -- Версия сервера: 8.2.0
 -- Версия PHP: 8.1.28
 
@@ -20,6 +20,37 @@ SET time_zone = "+00:00";
 --
 -- База данных: `build_house`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `houses`
+--
+
+CREATE TABLE `houses` (
+  `id` int NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `material` varchar(64) NOT NULL,
+  `floor_count` int NOT NULL,
+  `bedrooms_count` int NOT NULL,
+  `sanitary_unit` int NOT NULL,
+  `comfort` int NOT NULL,
+  `images` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `request`
+--
+
+CREATE TABLE `request` (
+  `id` int NOT NULL,
+  `houses_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  `date` varchar(32) NOT NULL,
+  `request_status` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
@@ -48,6 +79,20 @@ INSERT INTO `users` (`id`, `login`, `name`, `surname`, `phone`, `password`) VALU
 --
 
 --
+-- Индексы таблицы `houses`
+--
+ALTER TABLE `houses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `request`
+--
+ALTER TABLE `request`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `houses_id` (`houses_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
@@ -58,10 +103,33 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT для таблицы `houses`
+--
+ALTER TABLE `houses`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `request`
+--
+ALTER TABLE `request`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `request`
+--
+ALTER TABLE `request`
+  ADD CONSTRAINT `request_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `request_ibfk_2` FOREIGN KEY (`houses_id`) REFERENCES `houses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
